84a0423223ba53b43cbac42fcac3ffae14e1dbd6
Satyaki Sinha <94979394+SatyakiSinha2000@users.noreply.github.com>
2026-07-21 23:28:17 +0530

    Add main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+window.React = React;
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
